3adb74249abeda68035df87a7511a494de5ab174
miraj <mirajhowlader9999@gmail.ocm>
2024-12-05 01:12:48 +0600

    create a header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn'
 import SignOut from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './componets/Header'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route  path='/' element={<Home/>}/>
         <Route  path='/sign-in' element={<SignIn/>}/>
